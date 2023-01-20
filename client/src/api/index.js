0bb8947661f4847338ce5d2ea7e8cb6345ac6bf6
Natalia Kostka <natalia7kostka@gmail.com>
2023-01-20 11:24:01 -0700

    changed url on client side

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8000/posts';
+const url = 'https://backend-memories-dpz6.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost); 
